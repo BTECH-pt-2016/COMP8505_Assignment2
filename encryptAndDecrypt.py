402d94eb21d0d7eabd70920a58e73dc6a8f8f80a
mao <bd0421@gmail.com>
2016-05-10 21:53:36 -0700

    added file name

--- encryptAndDecrypt.py
+++ encryptAndDecrypt.py
@@ -3,34 +3,40 @@ from Crypto.Cipher import AES
 from Crypto import Random
 import binascii
 import os
-
+import sys
 
 BS = 16
-pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
-unpad = lambda s : s[:-ord(s[len(s)-1:])]
-
 
 def encrypt( text, key ):
+    pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
     text = pad(text)
     iv = Random.new().read( AES.block_size )
     cipher = AES.new( key, AES.MODE_CBC, iv )
-    return binascii.hexlify(base64.b64encode( iv + cipher.encrypt( text ) ))
+    return binascii.hexlify(iv + cipher.encrypt( text ) )
 
 def decrypt( encText, key ):
-    enc = base64.b64decode(binascii.unhexlify(encText))
+    unpad = lambda s : s[:-ord(s[len(s)-1:])]
+    enc = binascii.unhexlify(encText)
     iv = enc[:16]
     cipher = AES.new(key, AES.MODE_CBC, iv )
     return unpad(cipher.decrypt( enc[16:] ))
 
-filename = 'P4182010_s.jpg'
+if len(sys.argv) != 2:
+    print "please put file path"
+    print "Usage: python encryptAndDecrypt.py <filepath>"
+    sys.exit()
+else:
+    filename = sys.argv[1]
+
 with open(filename) as f:
     content = f.read()
 key = "abcabcabcabcabca"# the length has to be multiple of 16
-encrypted = encrypt(content, key)
-#print decrypt(encrypted , key)
+encrypted = encrypt(content+"^"+filename.split("/")[-1], key)
 
-writepath = 'decrypted/'+filename
+decrypted = decrypt(encrypted , key)
+filename2 = decrypted.split("^")[-1]
+writepath = 'decrypted/'+filename2
+content2 =  decrypted[:-len(filename2)+1]
 
-mode = 'a' if os.path.exists(writepath) else 'w'
-with open(writepath, mode) as f:
-    f.write(decrypt(encrypted , key))
+with open(writepath, 'w') as f:
+    f.write(content2)
